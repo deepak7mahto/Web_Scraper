61793027ed1b1d6aca1efbec18b23529c49f4f25
deepak7mahto <deepak7mahto@gmail.com>
2015-10-30 22:53:56 +0530

    push

--- Web_Scrapper_Generic/Web_Scrapper_Generic/Web_Scrapper_Generic.py
+++ Web_Scrapper_Generic/Web_Scrapper_Generic/Web_Scrapper_Generic.py
@@ -4,4 +4,4 @@ class Web_Scrapper_Generic(QtGui.QDialog,Web_Scrapper_Generic_UI.Ui_MainWindow):
     #initialization of main  windows 
     def __init__(self):
         QtGui.QDialog().__init__(self)
-        self.set
+        print "typed"
